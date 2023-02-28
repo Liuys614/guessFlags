1e7c183d0f2a5082f6d6e2e3dbef78c2f8fb3e28
ll <ll@gmail.com>
2023-02-28 12:58:17 -0500

    Initial Commit

---
+++ guessFlags/guessFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessFlagsApp.swift
+//  guessFlags
+//
+//  Created by Chih-Wei Lin on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct guessFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
